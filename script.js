9eb1f5de9a16edb4ef71987180c7b45b532cdb08
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-01-13 04:02:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const generateForm = document.querySelector(".generate-form");
+const generateBtn=generateForm.querySelector(".generate-btn");
+const imageGallery = document.querySelector(".image-gallery");
+
+const OPENAI_API_KEY = "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";
+let isImageGenerating = false;
+
+const updateImageCard = (imgDataArray) => {
+    imgDataArray.forEach((imgObject, index) => {
+        const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+        const imgElement = imgCard.querySelector("img");
+        const downloadBtn = imgCard.querySelector(".download-btn");
+
+        //set the image source to the AI-generated image data
+        const aiGeneratedImage= `data:image/jpeg;base64,${imgObject.b64_json}`;
+        imgElement.src = aiGeneratedImage;
+
+        imgElement.onload = () => {
+            imgCard.classList.remove("loading");
+            //will remove loading when image is generated
+          //  downloadBtn.setAttribute("href", aiGeneratedImage);
+           // downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
+        };
+    });
+};
+
+const generateAiImages = async (userPrompt, userImgQuantity) => {
+    try {
+        //sends a request to openAI API to generate images based on user inputs
+        const response = await fetch("POST https://api.openai.com/v1/images/generations", {
+            method: "POST",
+            headers: {
+                "content-Type": "application/json",
+                "Authorization": `Bearer ${OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                prompt: userPrompt,
+                n: parseInt(userImgQuantity),
+                size: "512x512",
+                response_format: "b64_json"
+            })
+
+        });
+
+        if (!response.ok) throw new Error("Failed to generate images! Please try again.");
+
+        const { data } = await response.json();
+        updateImageCard([...data]); //get data from the response
+        console.log(data);
+    } catch (error) {
+        alert(error.message);
+    } finally {
+        isImageGenerating = false;
+    }
+};
+
+const handleFormSubmission = (e) => {
+    e.preventDefault();
+    if (isImageGenerating) return;
+    isImageGenerating = true;
+    //get user input and image quantity value from the form
+    const userPrompt = e.srcElement[0].value;
+    const userImgQuantity = e.srcElement[1].value;
+    const imgCardMarkup = Array.from({ length: userImgQuantity }, () =>
+        `<div class="img-card loading">
+            <img src="images/loader.svg" alt="image">
+            <a href="#" class="download-btn">
+                <img src="images/download.svg" alt="download icon">
+            </a>
+        </div>`
+    ).join("");
+
+    imageGallery.innerHTML = imgCardMarkup;
+    generateAiImages(userPrompt, userImgQuantity);
+};
+
+generateForm.addEventListener("submit", handleFormSubmission);
