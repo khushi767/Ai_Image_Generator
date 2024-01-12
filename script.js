45fd83e07ab6eb258225550ce6930ac693e89d97
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-01-13 04:09:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn=generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "your-api-key";
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
